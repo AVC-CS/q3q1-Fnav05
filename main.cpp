097cf8640e647b682aaeb26483d064d50dae5362
Fnav05 <felvinnavarro10@gmail.com>
2026-03-12 00:51:25 -0700

    Runtime 1

--- main.cpp
+++ main.cpp
@@ -6,16 +6,23 @@ using namespace std;
 int main()
 {
     double a, b, c;
+    
     // TODO
+    cin >> a >> b >> c;
 
     // TODO
-    // TODO
-    // TODO
+    double disc = sqrt(b*b - 4*a*c);
+
+    double x1 = (-b + disc) / (2*a); // first root
+    double x2 = (-b - disc) / (2*a) ;// second root
 
     // TODO
-    // TODO
+    if (x2 > x1) {
+        swap(x1, x2);
+    }
 
     cout << setprecision(2) << fixed;
+    cout << x1 << " " << x2;
     // TODO
 
     return 0;
